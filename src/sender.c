9416e07d7e28f125a1746a88ee40a17b826765f7
BellonGuillaume <33180570+BellonGuillaume@users.noreply.github.com>
2018-10-18 18:57:31 +0200

    Update sender.c
    Fonctions premières 18-10-2018

---
+++ src/sender.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include "sender.h"
+
+struct __attribute__((__packed_)) pkt {
+    uint8_t window:5;
+    uint8_t tr:1;
+    uint8_t type:2;
+    uint8_t seqnum;
+    uint16_t length;
+    uint32_t timestamp;
+    uint32_t crc1;
+    char* payload;
+    uint32_t crc2;
+};
+
+int main (int argc, char* argv[])
+{
+	char* send_path = NULL;
+	int optionf = 0;
+	bool redirection = 0;
+	if (argc < 4){
+		fprintf(stderr, "Pas assez d'arguments\n");
+		return EXIT_FAILURE;
+	}
+	if (*argv[1].equals('-')){
+		if (*(argv[1]+1).equals('f')){
+			send_path = argv[2];
+			optionf = 1;
+		}
+		else {
+			fprintf(stderr, "Mauvaise option\n");
+			return EXIT_FAILURE;
+		}
+	}
+	else {
+		if (*argv[3].equals('<')) {
+			send_path = argv[4];
+			redirection = 1;
+		}
+	}
+	char* first_adresse = argv[(optionf*2)+1];
+	int port = atoi(argv[(optionf*2)+2]);
+	struct sockaddr_in6 addr;
+	const char *err = real_address(adresse, &addr);
+	if (err) {
+		fprintf(stderr, "Probleme avec l'hostname %s: %s\n", adresse, err);
+		return EXIT_FAILURE;
+	}
+	int sfd = encoder(&addr,port);
+	if(sfd < 0) {
+		fprintf(stderr, "Erreur dans l'encodage\n");
+		return EXIT_FAILURE;
+	}
+	if(envoyer(sfd, send_path, optionf, redirection) < 0) {
+		fprintf(stderr, "Erreur dans l'envoi\n");
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+
+}
+
+int encoder(struct sockaddr_in6 *addr, int port){
+}
+
+int envoyer(int sfd, char* send_path, int optionf, bool redirection){
+}
