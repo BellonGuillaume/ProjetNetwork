93ac80ecda6f26623a841e8fe9cc3a9170b44783
vahidbe <vahid.beyraghi@student.uclouvain.be>
2018-10-21 18:47:03 +0200

    test

--- test/test_suite.c
+++ test/test_suite.c
@@ -4,69 +4,36 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <string.h>
+#include "../src/window.c"
+#include "../src/packet.c"
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 
 int n=1;
-void test()
+
+void test_window()
 {
-	int fd=open("test.txt",O_CREAT|O_TRUNC|O_WRONLY,S_IRWXU|S_IRWXO);
-	for(int j=0;j<n;j++)
-	{
-		for(int i=0;i<10;i++)
-		{
-			char buf[1];
-			buf[0]=i+'0';
-			write(fd,buf,1);
-		}
-	}
-	pid_t pid=fork();
-	if(pid==0)
-	{
-		system("rm out.txt");
-		system("../receiver -f out.txt localhost 6565");
-		exit(0);
-	}
-	sleep(1);
-	system("../sender -f test.txt localhost 6565");
-	close(pid);
-	close(fd);
-	fd= open("out.txt",O_RDONLY);
-	int err=0;
-	if(fd<0)
-	{
-		err=1;
-	}
-	for(int j=0;j<n;j++)
-	{
-		for(int i=0;i<10;i++)
-		{
-			char buf[1];
-			read(fd,buf,1);
-			if(buf[0]!=(i+'0'))
-			{
-				//printf("%c - %c\n",buf[0],i+'0');
-				err=1;
-			}
-		}
-	}
-	close(fd);
-	//fprintf(stderr,"%d\n",err);
-	CU_ASSERT(err==0);
+	int length = 10;
+	window_t* test = window_new(length);
+	CU_ASSERT(test != NULL);
+	CU_ASSERT(test->length == length && test->size_used == 0 && test->buffer != NULL);
+	pkt_t* pktest = pkt_new();
+	node_t* nodetest = node_new(pktest);
+	CU_ASSERT(nodetest!=NULL);
+	CU_ASSERT(nodetest->pkt == pktest && nodetest->seqnum == pkt_get_seqnum(pktest));
+	CU_ASSERT(window_add(test, pktest) >= 0);
+	CU_ASSERT(test->length == length && test->size_used == 1 && test->buffer != NULL);
+	node_del(nodetest);
+	CU_ASSERT(test->size_used == 0 && test->buffer != NULL);
+	window_del(test);
 }
 
 int main (int argc, char* argv[])
 {
-	if(argc>1)
-	{
-		n=atoi(argv[1]);
-	}
-	printf("Test envoi de %d kb.\n",n/100);
 	CU_initialize_registry();
-  CU_pSuite suite = CU_add_suite("test", 0, 0);
-  CU_add_test(suite, "test", test);
+  CU_pSuite suite = CU_add_suite("test_window", 0, 0);
+  CU_add_test(suite, "test2", test_window);
   CU_basic_set_mode(CU_BRM_VERBOSE);
   CU_basic_run_tests();
 	CU_cleanup_registry();
