d39e09f9c2bd32afc85968b55c890d9f1fada0de
vahidbe <vahid.beyraghi@student.uclouvain.be>
2018-11-01 17:25:10 +0100

    Tests avancés

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-all : sender receiver
+all : sender receiver tests
 
 sender: packet.o sender.o
 	gcc -o sender packet.o  sender.o -lz
 
 receiver: packet.o receiver.o
 	gcc -o receiver packet.o receiver.o -lz
+
+tests: test_prog.o packet.o
+	gcc -o tests test_prog.o packet.o -lz -lcunit
 
 packet.o: src/packet.c src/packet_interface.h
 	gcc -c src/packet.c -lz
@@ -14,5 +17,9 @@ receiver.o: src/receiver.c src/receiver.h src/socket_manipulation.c src/commonli
 
 sender.o: src/sender.c src/sender.h src/socket_manipulation.c src/commonlib.c src/window.c
 	gcc -c src/sender.c src/socket_manipulation.c src/commonlib.c src/window.c
+
+test_prog.o: test/test_prog.o src/window.c
+	gcc -c test/test_prog.c src/window.c -lcunit
+
 clean :
-	rm sender receiver packet.o receiver.o sender.o commonlib.o socket_manipulation.o window.o
+	rm sender receiver packet.o receiver.o sender.o commonlib.o socket_manipulation.o window.o tests test_prog.o
--- test/test_prog.c
+++ test/test_prog.c
@@ -6,12 +6,304 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include "../src/window.c"
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 
 int n=1000;
-void test_prog()
+void test_perfect()
 {
+	printf("Test envoi de %d kb sur une ligne parfaite.\n",n/100);
+	pid_t pid=fork();
+	if(pid==0)
+	{
+		system("rm out.txt");
+		system("./receiver -f out.txt localhost 6565 >&-");
+		exit(0);
+	}
+	sleep(1);
+	system("./sender -f test.txt localhost 6565");
+	close(pid);
+	int fd= open("out.txt",O_RDONLY);
+	int err=0;
+	if(fd<0)
+	{
+		err=1;
+		fprintf(stderr,"No output file created\n");
+		CU_ASSERT(err==0);
+	}
+	for(int j=0;j<n;j++)
+	{
+		for(int i=0;i<10;i++)
+		{
+			char buf[1];
+			read(fd,buf,1);
+			if(buf[0]!=(i+'0'))
+			{
+				err=1;
+			}
+		}
+	}
+	close(fd);
+	system("rm out.txt");
+	CU_ASSERT(err==0);
+}
+
+void test_err()
+{
+	printf("Test envoi de %d kb avec 25/100 erreurs de corruption.\n",n/100);
+	pid_t pid=fork();
+	if(pid==0)
+	{
+		system("rm out.txt");
+		system("./test/linksim/link_sim -e 25 -p 6565 -P 1341  >&- & ./receiver -f out.txt :: 1341 >&-");
+		exit(0);
+	}
+	sleep(1);
+	system("./sender -f test.txt localhost 6565");
+	close(pid);
+	int fd= open("out.txt",O_RDONLY);
+	int err=0;
+	if(fd<0)
+	{
+		err=1;
+		fprintf(stderr,"No output file created\n");
+		CU_ASSERT(err==0);
+	}
+	for(int j=0;j<n;j++)
+	{
+		for(int i=0;i<10;i++)
+		{
+			char buf[1];
+			read(fd,buf,1);
+			if(buf[0]!=(i+'0'))
+			{
+				err=1;
+			}
+		}
+	}
+	close(fd);
+	system("rm out.txt");
+	CU_ASSERT(err==0);
+}
+
+void test_loss()
+{
+	printf("Test envoi de %d kb avec 25/100 des paquets perdus.\n",n/100);
+	pid_t pid=fork();
+	if(pid==0)
+	{
+		system("rm out.txt");
+		system("./test/linksim/link_sim -l 25 -p 6565 -P 1341  >&- & ./receiver -f out.txt :: 1341 >&-");
+		exit(0);
+	}
+	sleep(1);
+	system("./sender -f test.txt localhost 6565");
+	close(pid);
+	int fd= open("out.txt",O_RDONLY);
+	int err=0;
+	if(fd<0)
+	{
+		err=1;
+		fprintf(stderr,"No output file created\n");
+		CU_ASSERT(err==0);
+	}
+	for(int j=0;j<n;j++)
+	{
+		for(int i=0;i<10;i++)
+		{
+			char buf[1];
+			read(fd,buf,1);
+			if(buf[0]!=(i+'0'))
+			{
+				err=1;
+			}
+		}
+	}
+	close(fd);
+	system("rm out.txt");
+	CU_ASSERT(err==0);
+}
+
+void test_delay()
+{
+	printf("Test envoi de %d kb avec un délai de 100ms et un jitter de 200ms.\n",n/100);
+	pid_t pid=fork();
+	if(pid==0)
+	{
+		system("rm out.txt");
+		system("./test/linksim/link_sim -d 100 -j 200 -p 6565 -P 1341  >&- & ./receiver -f out.txt :: 1341 >&-");
+		exit(0);
+	}
+	sleep(1);
+	system("./sender -f test.txt localhost 6565");
+	close(pid);
+	int fd= open("out.txt",O_RDONLY);
+	int err=0;
+	if(fd<0)
+	{
+		err=1;
+		fprintf(stderr,"No output file created\n");
+		CU_ASSERT(err==0);
+	}
+	for(int j=0;j<n;j++)
+	{
+		for(int i=0;i<10;i++)
+		{
+			char buf[1];
+			read(fd,buf,1);
+			if(buf[0]!=(i+'0'))
+			{
+				err=1;
+			}
+		}
+	}
+	close(fd);
+	system("rm out.txt");
+	CU_ASSERT(err==0);
+}
+
+void test_err_1mb()
+{
+	printf("Test envoi de %d kb avec 1/100 erreurs de corruption.\n",n/100);
+	pid_t pid=fork();
+	if(pid==0)
+	{
+		system("rm out.txt");
+		system("./test/linksim/link_sim -e 1 -p 6565 -P 1341  >&- & ./receiver -f out.txt :: 1341 >&-");
+		exit(0);
+	}
+	sleep(1);
+	system("./sender -f test.txt localhost 6565");
+	close(pid);
+	int fd= open("out.txt",O_RDONLY);
+	int err=0;
+	if(fd<0)
+	{
+		err=1;
+		fprintf(stderr,"No output file created\n");
+		CU_ASSERT(err==0);
+	}
+	for(int j=0;j<n;j++)
+	{
+		for(int i=0;i<10;i++)
+		{
+			char buf[1];
+			read(fd,buf,1);
+			if(buf[0]!=(i+'0'))
+			{
+				err=1;
+			}
+		}
+	}
+	close(fd);
+	system("rm out.txt");
+	CU_ASSERT(err==0);
+}
+
+void test_loss_1mb()
+{
+	printf("Test envoi de %d kb avec 1/100 des paquets perdus.\n",n/100);
+	pid_t pid=fork();
+	if(pid==0)
+	{
+		system("rm out.txt");
+		system("./test/linksim/link_sim -l 1 -p 6565 -P 1341 >&- & ./receiver -f out.txt :: 1341 >&-");
+		exit(0);
+	}
+	sleep(1);
+	system("./sender -f test.txt localhost 6565");
+	close(pid);
+	int fd= open("out.txt",O_RDONLY);
+	int err=0;
+	if(fd<0)
+	{
+		err=1;
+		fprintf(stderr,"No output file created\n");
+		CU_ASSERT(err==0);
+	}
+	for(int j=0;j<n;j++)
+	{
+		for(int i=0;i<10;i++)
+		{
+			char buf[1];
+			read(fd,buf,1);
+			if(buf[0]!=(i+'0'))
+			{
+				err=1;
+			}
+		}
+	}
+	close(fd);
+	system("rm out.txt");
+	CU_ASSERT(err==0);
+}
+
+void test_tot()
+{
+	printf("Test envoi de %d kb avec 1/100 erreurs de corruption, 1/100 de paquets perdus, 100ms de délai et 200ms de jitter.\n",n/100);
+	pid_t pid=fork();
+	if(pid==0)
+	{
+		system("rm out.txt");
+		system("./test/linksim/link_sim -e 1 -l 1 -d 100 -j 200 -p 6565 -P 1341  >&- & ./receiver -f out.txt :: 1341 >&-");
+		exit(0);
+	}
+	sleep(1);
+	system("./sender -f test.txt localhost 6565");
+	close(pid);
+	int fd= open("out.txt",O_RDONLY);
+	int err=0;
+	if(fd<0)
+	{
+		err=1;
+		fprintf(stderr,"No output file created\n");
+		CU_ASSERT(err==0);
+	}
+	for(int j=0;j<n;j++)
+	{
+		for(int i=0;i<10;i++)
+		{
+			char buf[1];
+			read(fd,buf,1);
+			if(buf[0]!=(i+'0'))
+			{
+				err=1;
+			}
+		}
+	}
+	close(fd);
+	system("rm out.txt");
+	CU_ASSERT(err==0);
+}
+
+void test_window()
+{
+	int length = 10;
+	window_t* test = window_new(length);
+	CU_ASSERT(test != NULL);
+	if(test==NULL)
+	return;
+	CU_ASSERT(test->length == length && test->size_used == 0 && test->buffer != NULL);
+	pkt_t* pktest = pkt_new();
+	node_t* nodetest = node_new(pktest);
+	CU_ASSERT(nodetest!=NULL);
+	if(nodetest==NULL)
+	return;
+	CU_ASSERT(nodetest->pkt == pktest && nodetest->seqnum == pkt_get_seqnum(pktest));
+	CU_ASSERT(window_add(test, pktest) >= 0);
+	CU_ASSERT(test->length == length && test->size_used == 1 && test->buffer != NULL);
+	window_remove(test,nodetest->seqnum);
+	CU_ASSERT(test->size_used == 0 && test->buffer != NULL);
+	window_del(test);
+	test=NULL;
+}
+
+int main (int argc, char* argv[])
+{
+	system("fuser -k 1341/udp");
+	system("fuser -k 6565/udp");
+	system("rm test.txt");
 	int fd=open("test.txt",O_CREAT|O_TRUNC|O_WRONLY,S_IRWXU|S_IRWXO);
 	for(int j=0;j<n;j++)
 	{
@@ -22,63 +314,47 @@ void test_prog()
 			write(fd,buf,1);
 		}
 	}
-	pid_t pid=fork();
-	if(pid==0)
-	{
-		system("rm out.txt");
-		system("./receiver -f out.txt localhost 6565");
-		exit(0);
-	}
-	sleep(1);
-	system("./sender -f test.txt localhost 6565");
-	close(pid);
 	close(fd);
-	fd= open("out.txt",O_RDONLY);
-	int err=0;
-	if(fd<0)
-	{
-		err=1;
-		fprintf(stderr,"\n Do not forget to make ../\n\n");
-		return;
-	}
+	CU_initialize_registry();
+  CU_pSuite suite = CU_add_suite("Tests du programme avec 10kb", 0, 0);
+  CU_add_test(suite, "test1", test_perfect);
+	CU_add_test(suite, "test2", test_err);
+	CU_add_test(suite, "test3", test_loss);
+	CU_add_test(suite, "test3", test_delay);
+	CU_pSuite suite2 = CU_add_suite("Tests du programme avec 1mb", 0, 0);
+  CU_add_test(suite2, "test1", test_perfect);
+	CU_add_test(suite2, "test2", test_err_1mb);
+	CU_add_test(suite2, "test3", test_loss_1mb);
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  CU_basic_run_suite(suite);
+
+	n=n*25;
+	system("fuser -k 1341/udp");
+	system("fuser -k 6565/udp");
+	system("rm test.txt");
+	fd=open("test.txt",O_CREAT|O_TRUNC|O_WRONLY,S_IRWXU|S_IRWXO);
 	for(int j=0;j<n;j++)
 	{
 		for(int i=0;i<10;i++)
 		{
 			char buf[1];
-			read(fd,buf,1);
-			if(buf[0]!=(i+'0'))
-			{
-				//printf("%c - %c\n",buf[0],i+'0');
-				err=1;
-			}
+			buf[0]=i+'0';
+			write(fd,buf,1);
 		}
 	}
 	close(fd);
-	//fprintf(stderr,"%d\n",err);
-	CU_ASSERT(err==0);
-}
+	CU_basic_run_suite(suite2);
 
-int main (int argc, char* argv[])
-{
-	/*
-	if(argc==2)
-	{
-		n=atoi(argv[1]);
-	}
+	system("fuser -k 1341/udp");
+	system("fuser -k 6565/udp");
+	CU_pSuite suite3 = CU_add_suite("Test avec mauvaise connection du programme", 0, 0);
+	CU_add_test(suite3, "test1", test_tot);
+	CU_basic_run_suite(suite3);
 
-	else
-	{
-		fprintf(stderr,"\nUsage :\n\n	./test1 size\n	- size being the number of times the sequence 0123456789 will be sent\n\n");
-		return -1;
-	}
-	*/
+	system("rm test.txt");
 
-	printf("Test envoi de %d kb.\n",n/100);
-	CU_initialize_registry();
-  CU_pSuite suite = CU_add_suite("test_prog", 0, 0);
-  CU_add_test(suite, "test1", test_prog);
-  CU_basic_set_mode(CU_BRM_VERBOSE);
-  CU_basic_run_tests();
+	CU_pSuite suite_window = CU_add_suite("Tests fonctions de window.c", 0, 0);
+	CU_add_test(suite_window, "test1", test_window);
+	CU_basic_run_suite(suite_window);
 	CU_cleanup_registry();
 }
