e63f8346406cf4aa6c1ffe7c11a75ead729ce1c2
BellonGuillaume <guillaume.bellon@student.uclouvain.be>
2018-11-01 21:18:01 +0100

    Pre-post des .h

--- src/receiver.h
+++ src/receiver.h
@@ -18,4 +18,48 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/*
+ * Cree un pkt de type ack, l'initialise et l'envoie sur
+ * un socket via le sfd donne.
+ *
+ * @sfd: Le Socket File Director sur lequel envoyer l'ack
+ * @seqnum: Le nouveau numero de sequence que le receiver s'attend a recevoir
+ * @return: -1 en cas d'erreur
+ *          0 si tout s'est bien passe
+ */
+int send_ack(int sfd, int seqnum);
+
+/*
+ * Cree un pkt de type nack, l'initialise et l'envoie sur
+ * un socket via le sfd donne.
+ *
+ * @sfd: Le Socket File Director sur lequel envoyer l'ack
+ * @seqnum: Le nouveau numero de sequence que le receiver s'attend a recevoir
+ * @return: -1 en cas d'erreur
+ *          0 si tout s'est bien passe
+ */
+int send_nack(int sfd, int seqnum);
+
+/*
+ * Copie le payload du pkt donne en argument et l'ecrit sur la sortie demande.
+ *
+ * @fd: Le File Director de sortie
+ * @pkt: Le paquet duquel prendre le payload
+ * @return: 0 si tout s'est bien passe
+ */
+int process_data(int fd, pkt_t* pkt);
+
+/*
+ * Recoit les pkt d'un sender et les traite pour extraire
+ * leur payload sur la sortie demande. Renvoie aussi des ack
+ * et des nack au sender en fonction du pkt recu. Code implemente
+ * en selective repeat.
+ *
+ * @sfd: Le Socket File Director via lequel recevoir/envoyer des pkt
+ * @filename: Le path name du fichier de sortie
+ * @optionf: booleen disant si l'option f est presente et
+ *           si la sortie se fait sur un fichier et non sur stdin
+ * @return: -1 en cas d'erreur
+ *          0 si toutes les donnees ont bien ete traitees
+ */
 int receive_data(int sfd, char* filename, int optionf);
--- src/sender.h
+++ src/sender.h
@@ -21,4 +21,20 @@
 #include <sys/time.h>
 #include "window.c"
 
+/*
+ * Ouvre le fichier d'entree (peut etre stdin) et le copie sur le
+ * payload de pkt et les envoie via le sfd passe en argument.
+ * Cree une window stockant les pkt envoye et dont on a pas recu d'accuse
+ * de reception et renvoie un pkt apres un temps RTT de 4 secondes
+ * si l'accuse de reception n'est pas recu. Envoie egalement un pkt_t
+ * de EOF et se deconnecte apres 10 secondes si l'ack de deconnexion
+ * n'est pas recu.
+ *
+ * @sfd: Le Socket File Director via lequel envoyer/recevoir des pkt
+ * @filename: Le path name du fichier d'entree
+ * @optionf: booleen disant si l'option f est presente et
+ *           si la sortie se fait sur un fichier et non sur stdin
+ * @return: -1 en cas d'erreur
+ *          0 si toutes les donnees ont bien ete traitees
+ */
 int send_data(int sfd, char* filename, int optionf);
